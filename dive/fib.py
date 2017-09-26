e7f89855aab8137bf0795f267681d56acab655f7
Praveen <praveen@testpress.in>
2017-09-26 20:18:54 +0530

    deep dive and learn python hard way files

---
+++ dive/fib.py
@@ -0,0 +1,12 @@
+def fib(n):
+    print "n = ",n
+    if n > 1 :
+        return n * fib(n-1)
+    else:
+        print "end of line"
+        return 1
+
+
+
+if __name__=="__main__":
+    print fib(5)
